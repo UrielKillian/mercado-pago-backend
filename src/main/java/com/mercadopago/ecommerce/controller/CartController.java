2c438c3a4a89d606b747eb7629235eaf14f989eb
UrielKillian <urielkillian2607@hotmail.com>
2021-02-18 18:43:25 -0500

    Update CartController.java

--- src/main/java/com/mercadopago/ecommerce/controller/CartController.java
+++ src/main/java/com/mercadopago/ecommerce/controller/CartController.java
@@ -92,6 +92,7 @@ public class CartController {
             backUrls.setFailure("https://ecommerce-mercado-pago-backend.herokuapp.com/paymentCondition");
             backUrls.setPending("https://ecommerce-mercado-pago-backend.herokuapp.com/paymentCondition");
             backUrls.setSuccess("https://ecommerce-mercado-pago-backend.herokuapp.com/paymentCondition");
+            preference = preference.setAutoReturn(Preference.AutoReturn.approved);
             return ResponseEntity.ok(preference.save().getInitPoint());
         } catch (MPException e){
             throw new ResourceNotFoundException("Something was wrong", e);
